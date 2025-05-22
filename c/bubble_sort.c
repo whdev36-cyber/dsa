c084430e7920de84fd4364e8c2ca33f1d1a1e905
WCyber <whdev36@gmail.com>
2025-05-22 07:37:12 +0500

    feat: implement bubble sort algorithm with print function

---
+++ c/bubble_sort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int data[] = {-2, 45, 0, 11, -9};
+    int size = sizeof(data) / sizeof(data[0]);
+    bubbleSort(data, size);
+    printArray(data, size);
+}
